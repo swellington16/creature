55b1f1a28eba685c53ba97d8d1b076e2b500bf6d
unknown <Sean@.(none)>
2013-09-24 17:09:40 -0500

    Starting repo

---
+++ org/swellington16/creature/AbstractCreature.java
@@ -0,0 +1,50 @@
+package org.swellington16.creature;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public abstract class AbstractCreature
+{
+    protected String name; //name of creature
+    protected int feed_val; //keeps track of when it should feed
+    protected int grow_val; //keeps track of its growth
+    final protected int STARVE_VAL = 10; //value of feed_val at which creature will die
+    final protected int DEATH_VAL = 10; //value of grow_val at which creature will die
+    final protected int TEETH_VAL = 2; //value of grow_val at which creature will get teeth
+    final protected int CLAWS_VAL = 5; //value of grow_val at which creature will get claws
+    final protected int TONGUE_VAL = 7; //value of grow_val at which creature will get a tongue
+    protected boolean isDead; //Is the creature dead?
+    protected boolean hasEyes; //Does the creature have eyes?
+    protected boolean hasMouth; //Does the creature have a mouth?
+    protected boolean hasTeeth; //Does the creature have teeth?
+    protected boolean hasClaws; //Does the creature have claws?
+    protected boolean hasTongue; //Does the creature have a tongue?
+    protected BufferedReader nose; //The creature's "nose"
+    protected BufferedWriter mouth; //The creature's "mouth"
+    
+    public AbstractCreature()
+    {
+    }
+    
+    public void feed()
+    {
+    }
+    
+    public void starve()
+    {
+    }
+    
+    public void grow()
+    {
+    }
+    
+    public void excrete()
+    {
+    }
+    
+    public void speak()
+    {
+    }
+}
---
+++ org/swellington16/creature/Creature.java
@@ -0,0 +1,210 @@
+package org.swellington16.creature;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Scanner;
+
+public class Creature extends AbstractCreature implements Runnable
+{
+
+    public Creature()
+    {
+        this.name = "TEST_CREATURE";
+        this.feed_val = 0;
+        this.grow_val = 0;
+        this.isDead = false;
+        this.hasEyes = true;
+        this.hasMouth = true;
+        this.hasTeeth = false;
+        this.hasClaws = false;
+        this.hasTongue = false;
+    }
+    
+    public int getFeedVal()
+    {
+        return feed_val;
+    }
+    
+    public int getGrowVal()
+    {
+        return grow_val;
+    }
+    
+    public boolean hasDied()
+    {
+        return isDead;
+    }
+    
+    public void starve()
+    {
+        this.feed_val += 1;
+        if(feed_val == STARVE_VAL)
+        {
+            this.isDead = true;
+            System.out.println("The creature is dead.");
+            System.out.println("Cause of death: Starvation");
+        }
+    }
+
+    public void feed()
+    {
+        String filename = "food.foodtray";
+        String food = "";
+        try
+        {
+            this.nose = new BufferedReader(new FileReader(filename));
+            String line = null;
+
+            while((line = nose.readLine()) != null)
+            {
+                food = food + line;
+            }
+
+            this.nose.close(); //stop sniffing
+        }
+        catch(IOException e)
+        {
+            System.out.println("Food tray cannot be opened/found for reading...");
+        }
+
+        try
+        {
+            if(!(food.equals("")))
+            {
+                this.mouth = new BufferedWriter(new FileWriter(filename));
+                this.mouth.write(""); //feed
+                this.mouth.close(); //stop feeding
+                this.feed_val = 0; //reset feed value
+                this.excrete();
+                this.grow(); 
+            }
+            else
+            {
+                System.out.println("...........");
+            } 
+        }
+        catch(IOException e1)
+        {
+            System.out.println("Food tray cannot be opened/found for writing...");
+        }   
+    }
+
+    public void grow()
+    {
+        this.grow_val += 1;
+        if(grow_val == TEETH_VAL)
+        {
+            this.hasTeeth = true;
+            System.out.println("The creature now has teeth...");
+        }
+        if(grow_val == CLAWS_VAL)
+        {
+            this.hasClaws = true;
+            System.out.println("The creature now has claws...");
+        }
+        if(grow_val == TONGUE_VAL)
+        {
+            this.hasTongue = true;
+            System.out.println("The creature now has a tongue...");
+        }
+        if(grow_val == DEATH_VAL)
+        {
+            this.isDead = true;
+            System.out.println("The creature is dead.");
+            System.out.println("Cause of death: Natural growth");
+        }
+    }
+
+    public void excrete()
+    {
+        String filename = "waste/dump"+this.grow_val+".txt";
+        try
+        {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            writer.write("creature faeces");
+            writer.close();
+        }
+        catch(IOException ex)
+        {
+            System.out.println("Creature cannot take a dump...");
+        }
+    }
+
+    public void move()
+    {
+        this.run();
+    }
+
+    class Movement
+    {
+        Timer timer;
+        Creature creature;
+        boolean mustFeed;
+        final int SECONDS = 1;
+
+        public Movement(Creature c)
+        {
+            this.creature = c;
+            this.timer = new Timer();
+            this.timer.schedule(new MoveTask(), SECONDS*1000);
+        }
+
+        class MoveTask extends TimerTask
+        {
+            public void run()
+            {
+                System.out.println("The creature needs to feed...");
+                timer.cancel();
+                promptFeeding();
+            }
+        }
+        
+        //"Fill the foodtray file for the creature to feed on
+        public void fillTray()
+        {
+            try
+            {
+                BufferedWriter filler = new BufferedWriter(new FileWriter("food.foodtray"));
+                filler.write("dghfskfsgfj");
+                filler.close();
+                System.out.println("Food tray is filled...");
+            }
+            catch(IOException ef)
+            {
+                System.out.println("The food tray cannot be filled...");
+            }
+        }
+        
+        public void promptFeeding()
+        {
+            this.creature.starve();
+            if(!(this.creature.isDead))
+            {
+                this.fillTray(); //Fill the food tray for the creature
+                this.creature.feed();
+                if(!(this.creature.isDead))
+                {
+                    this.creature.move();
+                }
+            }
+        }
+    }
+
+    
+    public void run()
+    {
+        new Movement(this);
+        System.out.println("The creature is moving...");
+    }
+
+    public static void main()
+    {
+        Creature creature = new Creature();
+        creature.move();
+    }
+}
